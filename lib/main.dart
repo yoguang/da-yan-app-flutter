f3296c41848146e6451d3c6f440a858d61e072fe
yoguang <wygdancer@aliyun.com>
2023-12-14 13:31:37 +0800

    feat(数据持久化): 使用shared_preferences插件实现本地数据持久化

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,20 @@
+import 'package:da_yan_app/utils/local_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:fl_amap_map/fl_amap_map.dart';
-import './pages/home_page.dart';
-import './pages/community_page.dart';
-import './pages/my_page.dart';
-import './pages/bottom_sheet_view.dart';
-import './widgets/location.dart';
-import './pages/bluetooth_view.dart';
-import './pages/device_finder_view.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
+import 'pages/home/home_view.dart';
+import 'pages/community/community_view.dart';
+import 'pages/personal/personal_view.dart';
+import 'pages/bluetooth/bluetooth_view.dart';
+import 'pages/bluetooth/bluetooth_model.dart';
+import 'models/location_model.dart';
 
+void main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   // 设置高德地图 APIKey
   setAMapKey(
     iosKey: 'f10d6b1d907e53d9e7e5e0a14a99c224',
@@ -21,7 +25,16 @@ void main() {
   ).then((value) {
     debugPrint('高德地图ApiKey设置$value');
   });
-  runApp(const MyApp());
+  await LocalStorage.init();
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => BluetoothDeviceModel()),
+        ListenableProvider(create: (_) => LocationModel()),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -35,25 +48,26 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
+      home: const MainPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MainPage> createState() => _MainPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MainPageState extends State<MainPage> {
+  late LocationModel? _locationModel;
   int _currentPageIndex = 0;
-
   List<Widget> tabPages = const [
+    HomeView(),
     BluetoothView(),
-    HomePage(),
-    AMapLocationPage(),
+    CommunityPage(),
+    PersonalView(),
   ];
 
   List<BottomNavigationBarItem> bottomNavBarItems = const [
@@ -66,13 +80,62 @@ class _MyHomePageState extends State<MyHomePage> {
       label: '社区',
     ),
     BottomNavigationBarItem(
-      icon: Icon(Icons.my_library_books),
+      icon: Icon(Icons.person),
       label: '我的',
     ),
   ];
 
+  @override
+  void initState() {
+    super.initState();
+    _locationModel = Provider.of<LocationModel>(context, listen: false);
+    FlutterNativeSplash.remove();
+    initLocation();
+  }
+
+  Future<bool> getPermission(Permission permission) async {
+    final PermissionStatus status = await permission.request();
+    if (!status.isGranted) {
+      // await openAppSettings();
+      return await permission.request().isGranted;
+    }
+    return status.isGranted;
+  }
+
+  /// 获取定位权限
+  Future<bool> get getPermissions async {
+    if (!await getPermission(Permission.location)) {
+      return false;
+    }
+    return true;
+  }
+
+  Future<void> getLocation() async {
+    if (!await getPermissions) return;
+    final location = await FlAMapLocation().getLocation(true);
+    debugPrint('getLocation===============>: ${location?.toMap()}');
+    if (location != null) {
+      _locationModel?.fromMap(location.toMap());
+    }
+  }
+
+  /// 初始化定位
+  Future<void> initLocation() async {
+    if (!await getPermissions) return;
+    await FlAMapLocation().initialize(AMapLocationOption());
+    getLocation();
+  }
+
+  // 连续定位
+  Future<void> startLocationState() async {
+    if (!await getPermissions) return;
+    final bool data = await FlAMapLocation().startLocationChanged(
+        onLocationChanged: (AMapLocation location) {
+      debugPrint('startLocationState================>: $location');
+    });
+  }
+
   void _bottomNavBarOnTap(int index) {
-    print('index: $index');
     setState(() {
       _currentPageIndex = index;
     });
@@ -85,6 +148,12 @@ class _MyHomePageState extends State<MyHomePage> {
         child: tabPages[_currentPageIndex],
       ),
       bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        showUnselectedLabels: true,
+        unselectedItemColor: Theme.of(context).unselectedWidgetColor,
+        selectedItemColor: Theme.of(context).primaryColor,
+        unselectedLabelStyle:
+            TextStyle(color: Theme.of(context).unselectedWidgetColor),
         items: bottomNavBarItems,
         currentIndex: _currentPageIndex,
         onTap: _bottomNavBarOnTap,
